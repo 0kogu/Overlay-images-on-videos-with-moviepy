884dc5a3c747b688276729ffa4cd0badf25605a0
0kogu <nascimentojosue2002@gmail.com>
2022-04-20 17:49:29 -0400

    sent main

---
+++ main.py
@@ -0,0 +1,17 @@
+import moviepy.editor as mp
+
+video = mp.VideoFileClip(r"C:\Users\USUARIO\Desktop\channels\travelling channel\Austria\austria1.mp4")
+
+
+
+logo = (mp.ImageClip(r"C:\Users\USUARIO\Desktop\logo.png")
+    .set_start(3)  
+    .set_end(10)  
+    .set_pos(("right","top")))
+    
+
+logo = logo.crossfadein(2.0)
+logo = logo.crossfadeout(2.0)
+
+final = mp.CompositeVideoClip([video, logo])
+final.write_videofile(r"C:\Users\USUARIO\Desktop\testes.mp4")
